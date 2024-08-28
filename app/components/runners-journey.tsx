10f2ac549b71cb5f920bffa1fa500796b1a3cacb
Akilhan <oqiljondadaxanov@gmail.com>
2024-08-28 20:17:01 +0500

    className added

--- app/components/runners-journey.tsx
+++ app/components/runners-journey.tsx
@@ -17,7 +17,7 @@ const RunnersJourney: React.FC = () => {
 
                     <div className="journey-card flex flex-col items-center gap-3 max-w-xs">
                         <div className="rounded-full p-6 bg-green-100 shadow-lg mb-4">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="44" height="44" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" class="lucide lucide-users"><path d="M16 21v-2a4 4 0 0 0-4-4H6a4 4 0 0 0-4 4v2" /><circle cx="9" cy="7" r="4" /><path d="M22 21v-2a4 4 0 0 0-3-3.87" /><path d="M16 3.13a4 4 0 0 1 0 7.75" /></svg>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="44" height="44" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-users"><path d="M16 21v-2a4 4 0 0 0-4-4H6a4 4 0 0 0-4 4v2" /><circle cx="9" cy="7" r="4" /><path d="M22 21v-2a4 4 0 0 0-3-3.87" /><path d="M16 3.13a4 4 0 0 1 0 7.75" /></svg>
                         </div>
                         <h1 className="text-gray-800 font-semibold text-2xl">Connect</h1>
                         <p className="text-center text-gray-600">Join clubs, make friends, and build your running community.</p>
